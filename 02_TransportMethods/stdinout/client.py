b98d6bed4402b35618e5e0c86a4f35ce62cbd37c
Markus <datamastery87@gmail.com>
2025-05-27 18:54:12 +0200

    remove initialize

--- 02_TransportMethods/stdinout/client.py
+++ 02_TransportMethods/stdinout/client.py
@@ -14,7 +14,6 @@ async def main() -> None:
 
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as session:
-            await session.initialize()
             res = await session.call_tool("add", {"a": 7, "b": 5})
             print("7 + 5 =", res.content[0].text)
 
