c4b16ef0dfadaf8810e0fc801786eb0efad6edc3
Markus <datamastery87@gmail.com>
2025-06-01 10:46:18 +0200

    change to app object

--- 10_Fastapi_Integration/app.py
+++ 10_Fastapi_Integration/app.py
@@ -9,4 +9,4 @@ app = FastAPI(lifespan=mcp_app.router.lifespan_context)
 app.mount("/mcpserver", mcp_app)
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000)
+    uvicorn.run(app=app, host="127.0.0.1", port=8000)
